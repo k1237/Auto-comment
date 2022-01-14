896f8a548af75a00d778c8ee45502666b336ee1a
k1237 <biz-kouta1237@outlook.jp>
2022-01-14 11:45:07 +0900

    compositionAPIを追加してみたがワーニング解決できず

--- test/com-footer.spec.js
+++ test/com-footer.spec.js
@@ -1,8 +1,12 @@
 // typescriptを使用する場合は別途設定が必要
 
 import { mount } from '@vue/test-utils'
-import { shallowMount } from '@vue/test-utils'
+import { createLocalVue,shallowMount } from '@vue/test-utils'
+import VueCompositionApi from '@vue/composition-api'
 import Footer from '../components/com-footer.vue'
+
+const localVue = createLocalVue()
+localVue.use(VueCompositionApi)
 
 describe('Footer.vue', () => {
   const wrapper = shallowMount(Footer)
@@ -20,9 +24,9 @@ describe('Footer.vue', () => {
     expect(wrapper.vm.$props.item).toEqual(['test1', 'test2','test3'])
   })
  
-
+  //  TypeError: _vm.fastClick is not a function
+  //  クリック後にemitされたかを検証したい
   test('クリック時にイベントハンドラが実行される', async () => {
-    const wrapper = mount(Footer)
-    await wrapper.find('#bp').trigger('fastClick')
+    wrapper.find('button').trigger('click');
   })
 })
